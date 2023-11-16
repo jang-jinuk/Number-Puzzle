98132497c35d752e7755a7866411fba98a84ebcb
MUDJANG <143267143+janginuk@users.noreply.github.com>
2023-11-16 14:59:27 +0900

    숫자 퍼즐게임 섞은 숫자 출력 수정 및 유효성 검사 구현
    1. 섞은 숫자 출력 코드 for문에서 for-each로 코드 간소화
    2. 입력받아 교환할 두 숫자 dowhile을 통해 범위설정

--- Main.java
+++ Main.java
@@ -21,17 +21,24 @@ public class Main {
                     --i;
             }
         }
-
-        for (int i = 0; i < 8; i++) { // 섞은 숫자 한줄로 출력
-            System.out.printf("%d ", szBuffer[i]);
+        System.out.print("[ ");
+        for (int num:szBuffer // 섞은 숫자 출력
+             ) {
+            System.out.print(num + " ");
         }
-        System.out.println("");
+        System.out.print("] \n");
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); //숫자 입력 받기
+        int nInput_1 = 0, nInput_2 = 0;
 
-        System.out.print("교환할 두 숫자 입력 >"); // 프롬포터 출력
-        int nInput_1 = scanner.nextInt();
-        int nInput_2 = scanner.nextInt();
 
+        do{
+            System.out.print("교환할 두 숫자 입력 > "); //입력 받은 숫자 유효성 검사
+            nInput_1 = scanner.nextInt();
+            nInput_2 = scanner.nextInt();
+            if(nInput_1 < 1 || nInput_1 > 8 || nInput_2 < 1 || nInput_2 > 8){
+                System.out.println("잘못 입력하셨습니다. 다시 입력해 주세요.\n");
+            }
+        }while(nInput_1 < 1 || nInput_1 > 8);
     }
 }
