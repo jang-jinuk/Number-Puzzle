fffc2b57a7d6caf172e0e5eb64977ba64f9fc160
MUDJANG <143267143+janginuk@users.noreply.github.com>
2023-11-17 15:19:48 +0900

    입력받은 두 가지 숫자 교환해주는 기능 구현
    입력 받은 두 숫자와 섞은 숫자 배열을 매개변수로 전달 해줘서 for문을 통해 배열의 값들 중 두가지 숫자와 같은 값이 있는 지 확인하고 일치하는 값이 있으면 해당 배열의 인덱스 값을 변수에 저장해 값을 교환해주는 기능을 구현함.

--- Main.java
+++ Main.java
@@ -13,10 +13,19 @@ public class Main {
         int turn = 1;
         System.out.printf("turn %d%n",turn); // 현재 턴 출력
 
-        MixNumber();
-        scanNumber();
+        int[] NumberPuzzle = new int[8];
+        NumberPuzzle =  MixNumber();
+        int[] nInputArray;
+        nInputArray = scanNumber();
+
+        int a, b;
+        a = nInputArray[0];
+        b = nInputArray[1];
+
+       NumberPuzzle = NumChange(a, b,NumberPuzzle);
+
     }
-    static void MixNumber(){
+    static int[] MixNumber(){
         int[] szBuffer = new int[8]; // 1 ~ 8까지 숫자를 무작위로 섞기
         for (int i = 0; i < 8; i++) {
             szBuffer[i] = (int) (Math.random() * 8 + 1);
@@ -31,8 +40,9 @@ public class Main {
             System.out.print(num + " ");
         }
         System.out.print("] \n");
+        return szBuffer;
     }
-    static void scanNumber(){
+    static int[] scanNumber(){
         int nInput_1 = 0, nInput_2 = 0;
 
         Scanner scanner = new Scanner(System.in); //숫자 입력 받기
@@ -44,5 +54,40 @@ public class Main {
             System.out.println("잘못 입력하셨습니다. 다시 입력해 주세요.\n");
         }
         }while(nInput_1 < 1 || nInput_1 > 8);
+
+        int[] nInput = new int[2];
+
+        nInput[0] = nInput_1;
+        nInput[1] = nInput_2;
+
+
+        return nInput;
+    }
+    static int[] NumChange(int a,int b, int[] c)
+    {
+        int[] ObjectNum = new int[8];
+        ObjectNum = c;
+
+        int changeBox_1 = 0, changeBox_2 = 0;
+        int tmp = 0;
+        for (int i = 0; i < 8; i++)
+        {
+            if (ObjectNum[i] == a)
+                changeBox_1 = i;
+        }
+        for (int j = 0; j < 8; j++)
+        {
+            if (ObjectNum[j] == b)
+                changeBox_2 = j;
+        }
+
+        tmp = changeBox_1;
+        changeBox_1 = changeBox_2;
+        changeBox_2 = tmp;
+
+        ObjectNum[changeBox_1] = b;
+        ObjectNum[changeBox_2] = a;
+
+        return ObjectNum;
     }
 }
